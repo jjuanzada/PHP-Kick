00cfb12294536d0da9c7e9d804661cbf2e81e948
jjuanzada <95050417+jjuanzada@users.noreply.github.com>
2022-04-17 22:18:14 -0300

    Desafio 06

---
+++ desafio_06/desafio_06.sql
@@ -0,0 +1,24 @@
+create database escola_db;
+
+use escola_db;
+
+create table aluno(
+    nome varchar(45),
+    cpf tinyint(11),
+    nascimento date,
+    genero char(1)
+);
+
+create table disciplina(
+    codigo tinyint(6),
+    nome varchar(20),
+    descricao text
+);
+
+create table professor(
+    nome varchar(45),
+    cpf tinyint(11),
+    nascimento date,
+    genero char(1),
+    formacao varchar(20)
+);
